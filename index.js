86478cb7b34b00d2c5a9ed3dcb5a9a483b40fa83
Ramaraja <r.ramaraja@yahoo.com>
2023-10-19 03:44:43 -0400

    Add userMessage event

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const express = require('express');
-const app = express();
-const PORT = 4000;
-const http = require('http').Server(app);
-const cors = require('cors');
+const { Server } = require("node:http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
 
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
+const PORT = 3001;
+const app = express();
+const http = Server(app);
+const io = socketIO(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
-// Generating responses based on LLM
-const handleResponse = (data, socket) => {
-    socket.emit('Sending messages to client');
-};
+app.use(cors());
 
-socketIO.on('connection', (socket) => {
-    console.log(`🤖: ${socket.id} User Connected`);
-  
-    socket.on('message', (data) => {
-        console.log(data);
-        socketIO.emit(handleResponse(data, socket));
-        socketIO.emit('messageResponse', data);
-    });
-    
-    socket.on('disconnect', () => {
-        console.log('🤖: User Disconnected');
-    });
+io.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on("userMessage", (data) => {
+    // Need to incorporate response from GPT-3
+    socket.emit("GPTResponse", [
+      data,
+      {
+        type: "AI",
+        name: "Persona",
+        text: "Generated text from GPT-3",
+        id: `${socket.id}${Math.random()}`,
+        socketID: socket.id,
+      },
+    ]);
   });
 
-app.use(cors());
+  socket.on("disconnect", () => {
+    console.log(`🔥: ${socket.id} user disconnected`);
+    socket.disconnect();
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.json({ message: "Reach out to ramaraja@vt.edu" });
+});
+
 http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
