c9b9af275b37a00cbee3b442f83a0312753f3e60
Ramaraja <r.ramaraja@yahoo.com>
2023-12-05 15:43:40 -0500

    Update model to GPT4

--- agent.js
+++ agent.js
@@ -10,7 +10,7 @@ class Agent {
   constructor() {
     this.model = new OpenAI({
       temperature: 0.9,
-      modelName: "gpt-3.5-turbo-1106",
+      modelName: "gpt-4-1106-preview",
     });
 
     this.chatPromptMemory = new ConversationSummaryBufferMemory({
